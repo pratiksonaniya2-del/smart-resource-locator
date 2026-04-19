c8f36b6d0ecf7004785a3fe320c967af3a664020
pratikSonaniya2-del <pratiksonaniya2@gmail.com>
2026-04-19 08:29:17 +0530

    fix import order

--- src/app/components/MapView.js
+++ src/app/components/MapView.js
@@ -1,13 +1,16 @@
 "use client";
 
-import { useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
 
-export default function MapView() {
+const LeafletMap = dynamic(
+  async () => {
+    const { MapContainer, TileLayer, Marker, Popup } =
+      await import("react-leaflet");
 
-  useEffect(() => {
+    const L = await import("leaflet");
+
     delete L.Icon.Default.prototype._getIconUrl;
 
     L.Icon.Default.mergeOptions({
@@ -18,17 +21,41 @@ export default function MapView() {
       shadowUrl:
         "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
     });
-  }, []);
 
-  const locations = [
-    { name: "Mata Mandir", pos: [23.233, 77.432] },
-    { name: "Ayodhya Nagar", pos: [23.259, 77.463] },
-    { name: "Bhopal Camp", pos: [23.250, 77.410] },
-  ];
+    return function Map() {
+      const locations = [
+        { name: "Mata Mandir", pos: [23.233, 77.432] },
+        { name: "Ayodhya Nagar", pos: [23.259, 77.463] },
+        { name: "Bhopal Camp", pos: [23.250, 77.410] },
+      ];
 
+      return (
+        <MapContainer
+          center={[23.2599, 77.4126]}
+          zoom={12}
+          style={{ height: "400px", width: "100%" }}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+          {locations.map((item, i) => (
+            <Marker key={i} position={item.pos}>
+              <Popup>{item.name}</Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      );
+    };
+  },
+  {
+    ssr: false,
+    loading: () => <p>Loading map...</p>,
+  }
+);
+
+export default function MapView() {
   return (
-    <div>
-      ...
+    <div className="rounded-2xl overflow-hidden border border-slate-700">
+      <LeafletMap />
     </div>
   );
 }
