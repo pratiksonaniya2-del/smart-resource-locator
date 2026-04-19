39c29ce4f6d1d63d07e4e488b6cba9f1936ca1f6
pratikSonaniya2-del <pratiksonaniya2@gmail.com>
2026-04-19 08:22:35 +0530

    fix import order

--- src/app/components/MapView.js
+++ src/app/components/MapView.js
@@ -4,20 +4,22 @@ import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-useEffect(() => {
-  delete L.Icon.Default.prototype._getIconUrl;
-
-  L.Icon.Default.mergeOptions({
-    iconRetinaUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-    iconUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-    shadowUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-  });
-}, []);
 
 export default function MapView() {
+
+  useEffect(() => {
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+      iconUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+      shadowUrl:
+        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    });
+  }, []);
+
   const locations = [
     { name: "Mata Mandir", pos: [23.233, 77.432] },
     { name: "Ayodhya Nagar", pos: [23.259, 77.463] },
@@ -25,20 +27,8 @@ export default function MapView() {
   ];
 
   return (
-    <div className="rounded-2xl overflow-hidden border border-slate-700">
-      <MapContainer
-        center={[23.2599, 77.4126]}
-        zoom={12}
-        style={{ height: "400px", width: "100%" }}
-      >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-        {locations.map((item, i) => (
-          <Marker key={i} position={item.pos}>
-            <Popup>{item.name}</Popup>
-          </Marker>
-        ))}
-      </MapContainer>
+    <div>
+      ...
     </div>
   );
 }
